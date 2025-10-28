deec79a5f93b0acbd3b9926eb57125b545e76b97
santish1802 <santiago497045@gmail.com>
2025-10-28 02:44:47 -0500

    feat: Inicializar proyecto, crear estructura base y configuración mínima.

---
+++ src/main/java/com/utp/nmtelecom/NmtelecomApplication.java
@@ -0,0 +1,13 @@
+package com.utp.nmtelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmtelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmtelecomApplication.class, args);
+	}
+
+}
